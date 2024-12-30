fe604520b4723fe3dcc5a31bf2e342e100703c7c
sakthicodes <sakthikumaran14122002@gmail.com>
2024-12-30 15:18:51 +0530

    server

--- BE_PRO/server.js
+++ BE_PRO/server.js
@@ -23,3 +23,9 @@ app.use("/", authRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
